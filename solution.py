4ea23ebe31fe5170c14b8e325c04e238b116d032
quickavin <sharmaavin@hotmail.com>
2017-07-20 16:27:06 +0530

    changes in search function

--- solution.py
+++ solution.py
@@ -135,27 +135,43 @@ def reduce_puzzle(values):
     return values
 
 def search(values):
-    "Using depth-first search and propagation, create a search tree and solve the sudoku."
+    # "Using depth-first search and propagation, create a search tree and solve the sudoku."
+    # # First, reduce the puzzle using the previous function
+    # values = reduce_puzzle(values)
+    #
+    # # Return values for recursion
+    # if values is False:
+    #     return False ## Failed earlier
+    # if all(len(values[s]) == 1 for s in boxes):
+    #     return values ## Solved
+    #
+    # # Choose one of the unfilled squares with the fewest possibilities
+    # unsolved_values = [box for box in values.keys() if len(values[box]) > 1]
+    # smallest_value = unsolved_values[0]
+    # for u in unsolved_values:
+    #     if values[smallest_value] > values[u]:
+    #         smallest_value = u
+    #
+    # # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+    # for digit in values[smallest_value]:
+    #     new_sudoku = values.copy()
+    #     new_sudoku[smallest_value] = digit
+    #     attempt = search(new_sudoku)
+    #     if attempt:
+    #         return attempt
+    "Using depth-first search and propagation, try all possible values."
     # First, reduce the puzzle using the previous function
     values = reduce_puzzle(values)
-    
-    # Return values for recursion
     if values is False:
-        return False ## Failed earlier
-    if all(len(values[s]) == 1 for s in boxes): 
-        return values ## Solved
-        
+        return False  ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes):
+        return values  ## Solved!
     # Choose one of the unfilled squares with the fewest possibilities
-    unsolved_values = [box for box in values.keys() if len(values[box]) > 1]
-    smallest_value = unsolved_values[0]
-    for u in unsolved_values:
-        if values[smallest_value] > values[u]:
-            smallest_value = u
-    
-    # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
-    for digit in values[smallest_value]:
+    n, s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and
+    for value in values[s]:
         new_sudoku = values.copy()
-        new_sudoku[smallest_value] = digit
+        new_sudoku[s] = value
         attempt = search(new_sudoku)
         if attempt:
             return attempt
